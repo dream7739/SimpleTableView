b0d9779a1349071a75bdc10baca00dc320f087a6
홍정민 <dream7739@naver.com>
2024-05-23 20:45:35 +0900

    쇼핑 컨트롤러 오토레이아웃 및 UI 구성

---
+++ SimpleTableView/ShoppingTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  ShoppingTableViewCell.swift
+//  SimpleTableView
+//
+//  Created by 홍정민 on 5/23/24.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    @IBOutlet var productLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+
+}
--- SimpleTableView/ShoppingTableViewController.swift
+++ SimpleTableView/ShoppingTableViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ShoppingTableViewController: UITableViewController {
-
+    
     @IBOutlet var productTextField: UITextField!
     @IBOutlet var addButton: UIButton!
     private var shoppingList: [String] = []
@@ -16,13 +16,22 @@ class ShoppingTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         addButton.layer.cornerRadius = 10
+        
+        productTextField.layer.cornerRadius = 10
+        let emptyView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 60))
+        productTextField.leftView = emptyView
+        productTextField.leftViewMode = .always
+        
     }
     
     @IBAction func clickAddButton(_ sender: UIButton) {
-        if productTextField.text?.trimmingCharacters(in: .whitespaces) != "" {
+        let productText = productTextField.text?.trimmingCharacters(in: .whitespaces)
+        
+        if productText != "" {
             shoppingList.append(productTextField.text!)
             tableView.reloadData()
         }
+       
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shoppingList.count
@@ -33,9 +42,9 @@ class ShoppingTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell")!
-        cell.textLabel?.text = shoppingList[indexPath.row]
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingCell") as? ShoppingTableViewCell else { return UITableViewCell() }
+        cell.productLabel?.text = shoppingList[indexPath.row]
         return cell
     }
-
+    
 }
